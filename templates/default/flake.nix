170efc80ed59d107d481778fe90776cf8e681147
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2026-04-19 14:34:27 +0200

    refactor(template): wire apps.default via mkBakeApp
    Template is consumers' first touchpoint; it should demonstrate the
    idiomatic `nix run .# -- <target>` UX rather than the two-step `nix
    build --print-out-paths | xargs docker buildx bake -f` pattern.
    
    Adds nixpkgs as an input (needed for pkgs.writeShellScript).

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -2,13 +2,19 @@
   description = "A docker-bake project using nix-docker-bake";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     bake.url = "github:sagikazarmark/nix-docker-bake";
   };
 
   outputs =
-    { bake, ... }:
+    {
+      nixpkgs,
+      bake,
+      ...
+    }:
     let
       system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
 
       scope = bake.lib.mkScope {
         config = { };
@@ -19,5 +25,11 @@
     in
     {
       packages.${system}.default = bake.lib.mkBakeFile scope.modules.app;
+
+      apps.${system}.default = bake.lib.mkBakeApp {
+        inherit pkgs;
+        module = scope.modules.app;
+        name = "app";
+      };
     };
 }
